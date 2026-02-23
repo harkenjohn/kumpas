b88251665561261121444ff7a46c8be2bfe2bf74
ADM4444 <amanayon860@gmail.com>
2026-02-23 19:27:44 +0800

    Added microphone permission pop-up

---
+++ Assets/Scripts/MicPermission.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Android;
+
+public class MicPermission : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+#endif
+    }
+}
